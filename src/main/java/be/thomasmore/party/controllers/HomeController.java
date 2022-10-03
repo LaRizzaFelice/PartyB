fcabdb08ffd420719d45937644a2cc06c771d3f1
Felice La Rizza <larizza.felice@gmail.com>
2022-10-03 21:43:30 +0200

    Les 03/10 :) nog later die avond

--- src/main/java/be/thomasmore/party/controllers/HomeController.java
+++ src/main/java/be/thomasmore/party/controllers/HomeController.java
@@ -5,10 +5,16 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 @Controller
 public class HomeController {
     private final int mySpecialNumber = 35;
     private final String[] venueNames = {"Zillion", "Carré", "Cherrymoon" , "Carat", "Boccacio" , "Cocorico", "Illusion"};
+
 
 
     @GetMapping(value = {"/", "/home", "/home/"})
@@ -31,15 +37,26 @@ public class HomeController {
         return "about";
     }
 
-    @GetMapping(value = {"/venuedetails/{venuename}"})
-    public String venuedetails(Model model, @PathVariable String venuename){
-        model.addAttribute("venuename",venuename);
-        return "venuedetails";
+
+    @GetMapping("/pay")
+    public String pay(Model model){
+        DateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+        Date today = new Date();
+        Calendar c = Calendar.getInstance();
+        c.setTime(today);
+        model.addAttribute("today",format.format(today));
+        c.add(Calendar.DATE,5);
+        Date paydate = c.getTime();
+        model.addAttribute("paydate", format.format(paydate));
+        return "pay";
     }
 
 
-
-
+    @GetMapping(value = {"/venuedetails","/venuedetails/{venuename}"})
+    public String venuedetails(Model model, @PathVariable (required = false) String venuename){
+        model.addAttribute("venuename",venuename);
+        return "venuedetails";
+    }
 
 
 }
